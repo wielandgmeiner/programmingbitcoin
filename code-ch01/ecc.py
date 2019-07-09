3cd3dbb525dae2eaa72d9b35e5eed9da350a8c48
U-BAWAG\E004028 <E004028@HVA00182.bawag.com>
2019-07-09 16:14:34 +0200

    Exercises chapter 1

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -23,7 +23,7 @@ class FieldElement:
 
     def __ne__(self, other):
         # this should be the inverse of the == operator
-        raise NotImplementedError
+        return not self == other
 
     # tag::source2[]
     def __add__(self, other):
@@ -39,7 +39,8 @@ class FieldElement:
         # self.num and other.num are the actual values
         # self.prime is what we need to mod against
         # We return an element of the same class
-        raise NotImplementedError
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
